852c6a29a5dd087e356899df9c4dead4e2f3440f
__darkhanbolatov__ <bolatovdarkhan25@gmail.com>
2020-08-16 18:48:06 +0600

    54 Eloquent local query scopes

--- app/Http/Controllers/Api/BookableAvailabilityController.php
+++ app/Http/Controllers/Api/BookableAvailabilityController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Bookable;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -13,11 +14,13 @@ class BookableAvailabilityController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request, Bookable $bookable)
     {
         $data = $request->validate([
             'from' => 'required|date|after_or_equal:now',
             'to' => 'required|date|after_or_equal:from'
         ]);
+
+        dd($bookable->bookings()->betweenDates($data['from'], $data['to'])->count());
     }
 }
