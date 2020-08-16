468e19efd55ee86d8e987b310112c1804d77615d
__darkhanbolatov__ <bolatovdarkhan25@gmail.com>
2020-08-16 18:26:18 +0600

    52 Single Action Controller

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('bookables', 'Api\BookablesController')->except(['create']);
+Route::get('bookables/{bookable}/availability', 'Api\BookableAvailabilityController')
+    ->name('bookable.availability.show');
